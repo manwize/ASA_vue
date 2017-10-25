adf828675eb452f863e968b4a1c17c822399c03a
mlwebdev <mlwebdeveloper@hotmail.com>
2017-10-25 18:17:01 -0400

    ASA start with routes
    ASA start with routes on navigation bar and on save and on save & continue buttons.
    Year drop-down dynamic in component.
    Services drop-down dynamic in component.

---
+++ asa_webpack_vue/src/components/user.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="container">
+		<h2>User Profile</h2>
+		<button class="btn btn-danger" 
+        @click="u_clearData">
+          Clear
+      </button>
+
+      <button class="btn btn-primary" 
+        @click="u_saveData">
+          Save
+      </button>
+      <button class="btn btn-success" 
+        @click="navigate_to_Vehicle">
+          Save & Continue
+      </button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'UserComponent',
+	data () {
+	  return {
+	  	json: null
+	  }
+	},
+  methods: {
+    u_clearData: function () {
+      alert('Clearing information')
+    },
+    u_saveData: function () {
+      alert('Saving information')
+    },
+    u_submitData: function () {
+      alert('Submitting information')
+    },
+    navigate_to_Vehicle: function () {
+      this.$router.push('/vehicle');
+    },
+     created: function () {
+      fetch("/data.json")
+        .then(r => r.json())
+        .then(json => {
+          this.json=json;
+        });
+    }
+  }
+}
+</script>
---
+++ asa_webpack_vue/src/store/store.js
@@ -0,0 +1,15 @@
+
+/*
+import Vue from 'vue';
+import Vuex from 'vuewx';
+
+Vue.use(Vuex);
+
+new Vuex.store({
+	state: {
+	}
+});
+*/
+
+// import { store } from './store.store'; in main.js
+// add store to main.js data{ }
